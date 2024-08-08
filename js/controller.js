38e010c690aea273e190bf911350681f023cea66
Alexey <alexey.anasov@yandex.by>
2024-08-08 22:58:46 +0300

    клик по счетчику в продуктах и начало метода для работы со счетчиком

--- js/controller.js
+++ js/controller.js
@@ -9,3 +9,16 @@ async function getAndRenderProducts() {
 }
 
 getAndRenderProducts();
+
+productsView.elements.productsContainer.addEventListener(
+  "click",
+  function (event) {
+    let action = event.target.dataset.action;
+
+    if (action === "plus" || action === "minus") {
+      const productId = +event.target.closest(".card").dataset.id;
+
+      productsModel.updateCounter(productId, action);
+    }
+  }
+);
--- js/products/model.js
+++ js/products/model.js
@@ -6,5 +6,23 @@ export default class Model {
   async loadProducts() {
     const response = await fetch("./js/products.json");
     this.products = await response.json();
+
+    for (const product of this.products) {
+      product.counter = 1;
+    }
+  }
+
+  updateCounter(id, action) {
+    const product = this.products.find((item) => item.id === id);
+
+    if (action === "plus") {
+      ++product.counter;
+    }
+
+    if (action === "minus" && product.counter > 1) {
+      --product.counter;
+    }
+
+    console.log(product);
   }
 }
