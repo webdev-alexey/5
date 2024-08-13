54ffb4223e79b4c2241078fa19e84db11945ee49
Alexey <alexey.anasov@yandex.by>
2024-08-13 21:44:05 +0300

    суммирование кол-ва существующих товаров в корзине

--- js/cart/model.js
+++ js/cart/model.js
@@ -4,7 +4,19 @@ export default class Model {
   }
 
   addToCart(product) {
-    const newProduct = JSON.parse(JSON.stringify(product));
-    this.cart.push(newProduct);
+    let productInCart;
+
+    productInCart = this.cart.find(function (productInCart) {
+      return productInCart.id === product.id;
+    });
+
+    if (productInCart) {
+      productInCart.counter = productInCart.counter + product.counter;
+    } else {
+      const newProduct = JSON.parse(JSON.stringify(product));
+      this.cart.push(newProduct);
+    }
+
+    console.log(this.cart);
   }
 }
